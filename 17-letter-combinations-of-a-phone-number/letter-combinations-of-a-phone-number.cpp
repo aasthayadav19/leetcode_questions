663bcc8acc2bfb3c33f7beb48e7e83423c4c1c0f
Aastha Yadav <aasthayadav052000@gmail.com>
2026-09-03 06:40:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (61.12%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
-    void fun(string& digits, int n, int idx, string& diary,
-             vector<string>& res) {
+    void fun(string& s, int n, int idx, string& diary, vector<string>& res,
+             unordered_map<char, string>& mp) {
 
-        // Base Case
         if (idx == n) {
             res.push_back(diary);
             return;
         }
 
-        // Digit ki corresponding letters
-        vector<string> mp = {"",    "",    "abc",  "def", "ghi",
-                             "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        string choice = mp[s[idx]];
 
-        string choice = mp[digits[idx] - '0'];
-
-        // Saare choices try karo
         for (int j = 0; j < choice.size(); j++) {
 
             diary.push_back(choice[j]);
 
-            fun(digits, n, idx + 1, diary, res);
+            fun(s, n, idx + 1, diary, res, mp);
 
             diary.pop_back();
         }
     }
-
     vector<string> letterCombinations(string digits) {
-        vector<string> res;
+        unordered_map<char, string> mp;
 
-        if (digits.empty())
-            return res;
+        mp['2'] = "abc";
+        mp['3'] = "def";
+        mp['4'] = "ghi";
+        mp['5'] = "jkl";
+        mp['6'] = "mno";
+        mp['7'] = "pqrs";
+        mp['8'] = "tuv";
+        mp['9'] = "wxyz";
+
+        int n = digits.size();
+
+        vector<string> res;
 
         string diary = "";
 
-        fun(digits, digits.size(), 0, diary, res);
+        int idx = 0;
+
+        fun(digits, n, idx, diary, res, mp);
 
         return res;
     }
